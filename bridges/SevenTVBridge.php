9369b37c03909f1a17db8be55bd5c12b07331f65
Andreas Bielawski <brawl98@outlook.com>
2019-06-18 22:44:37 +0200

    Update SevenTVBridge to use the sites directly since 7TV was discontinued

--- bridges/SevenTVBridge.php
+++ bridges/SevenTVBridge.php
@@ -2,25 +2,28 @@
 class SevenTVBridge extends BridgeAbstract {
 
     const MAINTAINER = 'Brawl';
-    const NAME = '7TV';
-    const URI = 'https://www.7tv.de/';
+    const NAME = 'ProSiebenSat.1';
+    const URI = 'https://www.sat1.de/';
     const CACHE_TIMEOUT = 1800; // 3600 = 1h
-    const DESCRIPTION = 'Returns the newest episode of a 7TV show.';
+    const DESCRIPTION = 'Returns the newest episode of a ProSiebenSat.1 show.';
     const PARAMETERS = array(
         array(
             'show' => array(
-                'name' => 'Show (e.g. "auf-streife")',
+                'name' => 'Show (from URL, e.g. "auf-streife")',
                 'type' => 'text',
                 'required' => true
             ),
-            'clip_type' => array(
-                'name' => 'Type',
+            'channel' => array(
+                'name' => 'TV channel',
                 'type' => 'list',
                 'values' => array(
-                    'Ganze Folgen' => 'ganze-folgen',
-                    'Clips' => 'alle-clips'
+                    'SAT.1' => 'https://www.sat1.de/',
+                    'ProSieben' => 'https://www.prosieben.de/',
+                    'ProSieben MAXX' => 'https://www.prosiebenmaxx.de/',
+                    'Kabel Eins' => 'https://www.kabeleins.de/',
+                    'SIXX' => 'https://www.sixx.de/'
                 ),
-                'defaultValue' => 'ganze-folgen'
+                'defaultValue' => 'https://www.sat1.de/'
             )
         )
     );
@@ -29,22 +32,9 @@ class SevenTVBridge extends BridgeAbstract {
     private $pageUrl = self::URI;
     
     public function getName(){
-        switch($this->getInput('clip_type')) {
-
-            case 'ganze-folgen':
-                if(!empty($this->showName)) {
-                    return $this->showName . ' - Ganze Folgen';
-                }
-                break;
-
-            case 'alle-clips':
-                if(!empty($this->showName)) {
-                    return $this->showName . ' - Alle Clips';
-                }
-                break;
-
+        if (!empty($this->showName)) {
+            return $this->showName . ' - Ganze Folgen';
         }
-
         return parent::getName();
     }
     
@@ -57,39 +47,37 @@ class SevenTVBridge extends BridgeAbstract {
         $show = $this->getInput('show');
         if (empty($show))
             returnClientError('Invalid show: ' . $show);
-        $clip_type = $this->getInput('clip_type');
+        $tvchannel = $this->getInput('channel');
 
         // Retrieve webpage
-        $pageUrl = self::URI . $show . '/' . $clip_type;
+        $pageUrl = $tvchannel . 'tv/' . $show;
         $html = getSimpleHTMLDOM($pageUrl)
-        or returnServerError('Could not request 7TV: ' . $pageUrl);
+            or returnServerError('Could not request: ' . $pageUrl);
         
         // Get show name
-        $showName = trim($html->find('span.format-header_title', 0)->plaintext);
+        $showName = trim($html->find('div.channel-title > h1', 0)->plaintext);
         $this->showName = $showName;
         $this->pageUrl = $pageUrl;
-
-        // Process articles
-        foreach($html->find('article.teaser') as $element) {
-
-            if(count($this->items) >= 10) {
-                break;
-            }
-
-            $article_title = $showName . ': "' . trim($element->find('h5.teaser-title', 0)->plaintext) . '"';
-            $article_uri = self::URI . substr($element->find('a', 0)->href, 1);
-            $article_thumbnail = $element->find('figure.teaser-figure', 0)->attr['data-src'];
-            $article_timestamp = strtotime($element->find('p.teaser-info', 0)->plaintext);
-
-            // Store article in items array
-            if (!empty($article_title) && !empty($article_uri)) {
-                $item = array();
-                $item['uri'] = $article_uri;
-                $item['title'] = $article_title;
-                $item['enclosures'] = array($article_thumbnail);
-                $item['timestamp'] = $article_timestamp;
-                $this->items[] = $item;
-            }
+        
+        // Process article
+        $element = $html->find('div.clickable-box-container')[0];
+        
+        $article_uri = $tvchannel . substr($element->find('a.clickable-box-link', 0)->href, 1);
+        $article_title = $showName . ': "' . trim($element->find('h3.teaser-headline', 0)->plaintext) . '"';
+        // Get article thumb from inline CSS
+        $article_thumbnail = trim($element->find('figure.teaser-img-squashed', 0)->attr['style']);
+        $article_thumbnail = str_replace("background-image:url(", "", $article_thumbnail);
+        $article_thumbnail = str_replace(");background-repeat:no-repeat", "", $article_thumbnail);
+        $article_thumbnail = str_replace("213x94", "996x440", $article_thumbnail);
+                
+        // Store article in items array
+        if (!empty($article_title) && !empty($article_uri)) {
+            $item = array();
+            $item['uri'] = $article_uri;
+            $item['title'] = $article_title;
+            $item['enclosures'] = array($article_thumbnail);
+            $this->items[] = $item;
         }
+
     }
 }
